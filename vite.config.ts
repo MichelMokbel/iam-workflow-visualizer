e1c340af4d0dda9a220a236abf9743dca0dd610e
Michel <michelmokbel@hotmail.com>
2025-12-04 19:00:17 +0100

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // CHANGE "iam-workflow-visualizer" to your repo name
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/iam-workflow-visualizer/', // very important for GitHub Pages
+  base: command === 'build' ? '/iam-workflow-visualizer/' : '/', // very important for GitHub Pages
   build: {
-    outDir: 'docs', // GitHub Pages will serve from /docs
+    outDir: '/', // GitHub Pages will serve from /docs
   },
-})
+}))
