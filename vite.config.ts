c543bb14a99623c3fdd9a5e331f0e4a7efb96b7f
Michel <michelmokbel@hotmail.com>
2025-12-04 18:42:38 +0100

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// CHANGE "iam-workflow-visualizer" to your repo name
+export default defineConfig({
+  plugins: [react()],
+  base: '/iam-workflow-visualizer/', // very important for GitHub Pages
+  build: {
+    outDir: 'docs', // GitHub Pages will serve from /docs
+  },
+})
